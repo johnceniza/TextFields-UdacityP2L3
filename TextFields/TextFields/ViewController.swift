80f00607dd0e741099153bb98cc405b9cbd2c426
John Ceniza <jc@johnceniza.com>
2015-07-02 00:23:59 -0400

    completed lockable textfield

--- TextFields/TextFields/ViewController.swift
+++ TextFields/TextFields/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var switcher: UISwitch!
     
     // Text Field Delegate objects
     let zipCodeDelegate = ZipCodeFieldDelegate()
@@ -24,12 +25,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        switcher.on = true
+        self.textField3.enabled = true
+        
         // Set the three delegates
         self.textField1.delegate = zipCodeDelegate
         self.textField2.delegate = cashFieldDelegate
         self.textField3.delegate = self //JC Notes: Using self as delegate because this specific text field requires a UILabel in the same view to be updated based on changes in this textfield!
     }
 
+    
+    @IBAction func textFieldSwitch(sender: UISwitch) {
+        if sender.on {
+            self.textField3.enabled = true
+        } else {
+            self.textField3.enabled = false
+            self.textField3.resignFirstResponder()
+        }
+    }
     
     // Text Field Delegate Methods
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
